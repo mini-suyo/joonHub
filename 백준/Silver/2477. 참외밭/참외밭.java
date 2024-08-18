3a178810ce0baa4402a9d5eb0434691ca0d33609
mini-suyo <sencroz@naver.com>
2024-08-18 22:22:39 +0900

    [Silver II] Title: 참외밭, Time: 168 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.java
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -6,14 +6,12 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int K = sc.nextInt(); // 1m당 자라는 참외의 갯수
-		int[] dir = new int[12];
-		int[] field = new int[12];
+		int[] dir = new int[6];
+		int[] field = new int[6];
 
 		for (int i = 0; i < 6; i++) { // 육각형을 만들 것!
 			dir[i] = sc.nextInt(); // 방향 설정
-			dir[i + 6] = dir[i]; // 예외사항 고려를 위해 복사에서 뒤에 붙힌다.
 			field[i] = sc.nextInt(); // 길이 설정
-			field[i + 6] = field[i]; // 예외사항 고려를 위해 복사에서 뒤에 붙힌다.
 		}
 
 		int maxLength = Integer.MIN_VALUE, maxWidth = Integer.MIN_VALUE;
@@ -34,9 +32,9 @@ public class Main {
 		int catchB = 0;
 
 		for (int i = 0; i < field.length; i++) {
-			if ((dir[i] == dir[i + 2]) && (dir[i + 1] == dir[i + 3])) { // 4칸반복될때가 들어간 부분
-				catchA = field[i + 1];
-				catchB = field[i + 2];
+			if ((dir[i % 6] == dir[(i + 2) % 6]) && (dir[(i + 1) % 6] == dir[(i + 3) % 6])) { // 4칸반복될때가 들어간 부분
+				catchA = field[(i + 1) % 6];
+				catchB = field[(i + 2) % 6];
 				break;
 			}
 		}

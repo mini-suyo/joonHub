0725558ef84a1cdcb6d8b63421011a337a7a791c
mini-suyo <sencroz@naver.com>
2024-08-26 14:06:58 +0900

    [Bronze V] Title: 윤년, Time: 168 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		if ((N % 4 == 0 && N % 100 != 0) || N % 400 == 0)
+			System.out.println(1);
+		else
+			System.out.println(0);
+
+	}
+
+}

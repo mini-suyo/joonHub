30e15ea486b063a54443c52e02552a30259ef7bc
mini-suyo <sencroz@naver.com>
2024-08-26 14:54:42 +0900

    [Bronze V] Title: 구구단, Time: 188 ms, Memory: 18516 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		for (int j = 1; j < 10; j++) {
+			System.out.println(N + " * " + j + " = " + N * j);
+		}
+
+	}
+
+}

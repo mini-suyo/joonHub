28b06a46de55ff3f16e0e0baa40d2eaaa826f340
mini-suyo <sencroz@naver.com>
2024-09-09 11:53:34 +0900

    [D2] Title: 스도쿠 검증, Time: 168 ms, Memory: 23,360 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,79 +1,72 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.HashSet;
 import java.util.Scanner;
- 
+
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
- 
-        for (int t = 0; t < T; t++) {
-            int[][] sudoku = new int[9][9];
-            int result = 1; // 출력값(기본 1: 맞음으로 설정)
- 
-            // 9x9 행렬에 들어갈 데이터 받기
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    sudoku[i][j] = sc.nextInt();
-                }
-            }
- 
-            // 가로줄 확인
-            PriorityQueue<Integer> pQwidth = new PriorityQueue<>(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return o1 - o2;
-                }
-            });
-            a: for (int i = 0; i < sudoku.length; i++) {
-                for (int j = 0; j < sudoku[0].length; j++) {
-                    pQwidth.add(sudoku[i][j]);
-                }
-                for (int j = 0; j < sudoku[0].length; j++) {
-                    if (pQwidth.poll() != j + 1) {
-                        result = 0;
-                        break a;
-                    }
-                }
-            }
- 
-            // 세로줄 확인
-            PriorityQueue<Integer> pQheight = new PriorityQueue<>();
-            a: for (int i = 0; i < sudoku.length; i++) {
-                for (int j = 0; j < sudoku[0].length; j++) {
-                    pQheight.add(sudoku[j][i]);
-                }
-                for (int j = 0; j < sudoku[0].length; j++) {
-                    if (pQheight.poll() != j + 1) {
-                        result = 0;
-                        break a;
-                    }
-                }
-            }
- 
-            // 3x3 확인
-            a: for (int i = 0; i < 9; i += 3) {
-                for (int j = 0; j < 9; j += 3) {
-                    int[] arr = new int[9];
-                    for (int k = 0; k < 3; k++) {
-                        for (int l = 0; l < 3; l++) {
-                            arr[k * 3 + l] = sudoku[i + k][j + l];
-                        }
-                    }
-                    Arrays.sort(arr);
-                    for (int k = 0; k < 9; k++) {
-                        if (arr[k] - 1 != k) {
-                            result = 0;
-                            break a;
-                        }
-                    }
-                }
-            }
- 
-            System.out.println("#" + (t + 1) + " " + result);
- 
-        }
- 
-    }
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		tcloop: for (int tc = 1; tc <= T; tc++) {
+			int[][] map = new int[9][9];
+			
+			for (int i = 0; i < map.length; i++) {
+				for (int j = 0; j < map.length; j++) {
+					map[i][j] = sc.nextInt();
+				}
+			}
+			boolean check  = true; //스도쿠 맞는지 확인여부 
+			
+			for (int r = 0; r < map.length; r++) {
+				
+			HashSet<Integer> wideset = new HashSet<>();
+			HashSet<Integer> verticalset = new HashSet<>();
+			
+				for (int c = 0; c < map.length; c++) {
+					if(!wideset.add(map[r][c])) {
+						check = false;
+						System.out.println("#"+ tc + " "+ 0);
+						continue tcloop;
+					}
+					
+					if(!verticalset.add(map[c][r])) {
+						System.out.println("#"+ tc + " "+ 0);
+						check = false;
+						continue tcloop;
+					}
+				}
+			}
+			
+			HashSet<Integer> set = new HashSet<>();
+			
+			int col = 0;
+			int row = 0;
+			final int RANGE = 3;
+			
+			for (int i = 0; i <map.length; i++) {
+				if(i % RANGE == 0 && i != 0) {
+					row += RANGE;
+					col = 0;
+				}
+				
+				for (int j = row; j <row+RANGE; j++) {
+					for (int k = col; k < col+RANGE; k++) {
+						if(!set.add(map[j][k])) {
+							check = false;
+							System.out.println("#"+ tc + " "+ 0);
+							continue tcloop;
+					}
+				}	
+				}
+				set = new HashSet<>();
+				col+= RANGE;
+			}
+			if(check) {
+				System.out.println("#"+tc+" "+ 1);
+			}
+		}
+
+	}
+
 }

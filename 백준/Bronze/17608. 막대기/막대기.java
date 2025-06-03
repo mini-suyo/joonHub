b27bac53dbbb4b1f5b653446d5958fbcb97218de
SEUNG YONG MIN <sencroz@naver.com>
2025-06-03 18:18:22 +0900

    [Bronze II] Title: 막대기, Time: 600 ms, Memory: 96184 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -4,19 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // arr 배열에서 마지막 수보다 큰수를 역순으로 찾는다.
+        // 찾을 때 마다 결과값을 1씩 증가 시키고 최댓값 변수로 저장한다.
+        // 
+
         int N = sc.nextInt();
         int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i]=sc.nextInt();
         }
         sc.close();
-        
+
+        int max = arr[N-1];
         int result = 1;
-        int lastNum = N-1;
-        for (int i = lastNum - 1; i >= 0; i--) {
-            if (arr[i] > arr[lastNum]) {
-                lastNum = i;
-                result ++;
+
+        for (int i = N - 2; i >= 0; i--) {
+            if (arr[i] > max) {
+                max = arr[i];
+                result++;
             }
         }
 

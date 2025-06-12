8abe2f2a4eb28c92b5445798a1d91dadb725456f
SEUNG YONG MIN <sencroz@naver.com>
2025-06-12 19:33:06 +0900

    [Silver II] Title: 유기농 배추, Time: 224 ms, Memory: 27788 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    static int M, N;
+    static int[][] field;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int t = 0; t < T; t++) {
+            M = sc.nextInt();
+            N = sc.nextInt();
+            int K = sc.nextInt();
+
+            field = new int[N][M];
+            visited = new boolean[N][M];
+
+            for (int i = 0; i < K; i++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                field[y][x] = 1;
+            }
+
+            int count = 0;
+            for (int y = 0; y < N; y++) {
+                for (int x = 0; x < M; x++) {
+                    if (field[y][x] == 1 && !visited[y][x]) {
+                        dfs(x, y);
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
+
+        sc.close();
+    }
+    
+    public static void dfs(int x, int y) {
+        visited[y][x] = true;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
+                if (field[ny][nx] == 1 && !visited[ny][nx]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+}

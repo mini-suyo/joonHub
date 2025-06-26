ba462499669b1967da664494666c26f882c40a1d
SEUNG YONG MIN <sencroz@naver.com>
2025-06-26 15:03:42 +0900

    [Silver IV] Title: 수 찾기, Time: 2040 ms, Memory: 186628 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,35 +1,23 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++)
-			arr[i] = sc.nextInt();
-		Arrays.sort(arr);
-		int m = sc.nextInt();
-		for (int i = 0; i < m; i++) {
-			int number = sc.nextInt();
-			int start = 0;
-			int end = n - 1;
-			while (true) {
-				int k = (start + end) / 2;
-				if (arr[k] == number) {
-					System.out.println(1);
-					break;
-				}
-				if (k <= end && k >= start && arr[k] > number)
-					end = k - 1;
-				else if (k <= end && k >= start && arr[k] < number)
-					start = k + 1;
-				else {
-					System.out.println(0);
-					break;
-				}
-			}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		}
-	}
+        int N = sc.nextInt();
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(sc.nextInt());
+        }
+
+        int M = sc.nextInt();
+        for (int i = 0; i < M; i++) {
+            int target = sc.nextInt();
+            if (set.contains(target)) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
 }

7da18f92d48c5479b2ab8af23e73256cd591d148
SEUNG YONG MIN <sencroz@naver.com>
2025-07-21 15:34:38 +0900

    [Silver IV] Title: 스택, Time: 456 ms, Memory: 32908 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            String order = sc.next();
+
+            if (order.equals("push")) {
+                int x = sc.nextInt();
+                stack.push(x);
+            } else if (order.equals("pop")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(stack.pop());
+                }
+            } else if (order.equals("size")) {
+                System.out.println(stack.size());
+            } else if (order.equals("empty")) {
+                if (stack.isEmpty()) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+            } else if (order.equals("top")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(stack.peek());
+                }
+            }
+        }
+    }
+}
